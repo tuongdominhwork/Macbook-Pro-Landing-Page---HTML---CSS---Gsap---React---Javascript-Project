c0092184d4a4084e45b95e52eadea18b6b263b78
Do Minh Tuong <minhtuong03@Dos-Mac-mini.local>
2026-02-26 18:03:01 +0300

    inital commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import { navLinks } from '../constants'
+
+export const NavBar = () => {
+  return (
+    <header>
+        <nav>
+            <img src="/logo.svg" alt="Apple logo" />
+            <ul>
+                {navLinks.map(({label}) => (
+                    <li key={label}>
+                        <a href="link.label">{label}</a>
+                    </li>
+                ))}
+            </ul>
+            <div className='flex-center gap-3'>
+                <button>
+                    <img src="/search.svg" alt="Search" />
+                </button>
+                <button>
+                    <img src="/cart.svg" alt="Cart" />
+                </button>
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default NavBar
